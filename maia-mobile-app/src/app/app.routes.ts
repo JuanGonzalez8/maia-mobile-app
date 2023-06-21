abc67d25ceaa5547a50b0f7cdeb630d5561a6d54
JuanGonzalez8 <125842196+JuanGonzalez8@users.noreply.github.com>
2023-06-20 21:28:39 -0600

    Nueva versión

---
+++ maia-mobile-app/src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  },
+  {
+    path: 'tab4',
+    loadComponent: () => import('./tab4/tab4.page').then( m => m.Tab4Page)
+  },
+  {
+    path: 'tab5',
+    loadComponent: () => import('./tab5/tab5.page').then( m => m.Tab5Page)
+  },
+  {
+    path: 'edit-profile',
+    loadComponent: () => import('./edit-profile/edit-profile.page').then( m => m.EditProfilePage)
+  },
+];
